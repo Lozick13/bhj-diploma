1da504d2074e01d94a30f0312e56369435227335
Ditriy Lozichnyy <dima_lozick13@mail.ru>
2024-01-25 12:42:23 +0700

    create modal

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -5,43 +5,56 @@
  * закрытие имеющихся окон
  * */
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor(element){
+	/**
+	 * Устанавливает текущий элемент в свойство element
+	 * Регистрирует обработчики событий с помощью Modal.registerEvents()
+	 * Если переданный элемент не существует,
+	 * необходимо выкинуть ошибку.
+	 * */
+	constructor(element) {
+		if (!element) {
+			throw new Error('Передан пустой элемент')
+		} else {
+			this.element = element
+			this.registerEvents()
+		}
+	}
 
-  }
+	/**
+	 * При нажатии на элемент с data-dismiss="modal"
+	 * должен закрыть текущее окно
+	 * (с помощью метода Modal.onClose)
+	 * */
+	registerEvents() {
+		const modalCloseBtns = this.element.querySelectorAll(
+			'[data-dismiss="modal"]'
+		)
+		modalCloseBtns.forEach(modalCloseBtn => {
+			modalCloseBtn.addEventListener('click', e => {
+				this.onClose(e)
+			})
+		})
+	}
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
-  registerEvents() {
-
-  }
-
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
-  onClose(e) {
-
-  }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
-  open() {
-
-  }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
-
-  }
+	/**
+	 * Срабатывает после нажатия на элементы, закрывающие окно.
+	 * Закрывает текущее окно (Modal.close())
+	 * */
+	onClose(e) {
+		e.preventDefault()
+		this.close()
+	}
+	/**
+	 * Открывает окно: устанавливает CSS-свойство display
+	 * со значением «block»
+	 * */
+	open() {
+		this.element.style.display = 'block'
+	}
+	/**
+	 * Закрывает окно: удаляет CSS-свойство display
+	 * */
+	close() {
+		this.element.style.removeProperty('display')
+	}
 }
